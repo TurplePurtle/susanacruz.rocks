4bd821ddc6aace69dc7d42654293415b38d2715e
TurplePurtle <santi256@gmail.com>
2015-09-08 21:51:32 -0500

    add load-ponies.js

---
+++ load-ponies.js
@@ -0,0 +1,21 @@
+function loadPonies(cfg) {
+  var cfg = {
+    "baseurl": "https://panzi.github.io/Browser-Ponies/",
+    "fadeDuration": 500,
+    "volume": 1,
+    "fps": 25,
+    "speed": 3,
+    "audioEnabled": false,
+    "showFps": false,
+    "showLoadProgress": false,
+    "dontSpeak": true,
+    "spawn": {
+      "sparkler": 1,
+      "twilight sparkle": 1
+    },
+    "autostart": true
+  };
+  BrowserPonies.setBaseUrl(cfg.baseurl);
+  BrowserPonies.loadConfig(BrowserPoniesBaseConfig);
+  BrowserPonies.loadConfig(cfg);
+}
